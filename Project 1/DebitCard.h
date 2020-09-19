3dd9e7ff5b11564684be6c2f17093510abc33088
rachellejackson <54724086+rachellejackson@users.noreply.github.com>
2020-09-19 14:06:02 -0700

    Create DebitCard.h

---
+++ Project 1/DebitCard.h
@@ -0,0 +1,25 @@
+#ifndef DEBITCARD_H
+#define DEBITCARD_H
+
+#include <string>
+#include "DynamicArray.h"
+#include "Transaction.h"
+
+class DebitCard
+{
+public:
+	DebitCard();
+	~DebitCard();
+
+	double Deposit(double deposit);				// used to add money to the DebitCard. Returns current ballance
+	double PostTransaction(int idNumber, std::string date, std::string description, double amount); // adds a new transaction to the transactions array. Returns account balance
+	bool ClearTransaction(int idNumber);	// marks the pending transaction in the transactions array as "cleared"
+	void DisplayActivity();					// properly displays the DebitCard data to the console screen
+
+	int totalTransactions;	// number of transactions posted for this DebitCard
+	double accountBalance;	// amount of money still in the account for this DebitCard
+	double totalDeposits;	// total amount of money added to this DebitCard
+private:
+	DynamicArray transactions;
+};
+#endif
