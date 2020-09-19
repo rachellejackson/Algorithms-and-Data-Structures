565ad9c372c8cb644a9c5772c528c0dc6c75a533
rachellejackson <54724086+rachellejackson@users.noreply.github.com>
2020-09-19 14:13:34 -0700

    Create Course.cpp

---
+++ Project 2/Course.cpp
@@ -0,0 +1,14 @@
+#include "Course.h"
+
+Course::Course(int courseNumber, std::string courseName, unsigned int credits, double grade) : tmp(this)
+{
+	// the initialization of the variable tmp MUST be done as shown above.
+	// this is for Unit Testing to work.
+
+	// your code goes here...
+	this->courseNumber = courseNumber;
+	this->courseName = courseName;
+	this->credits = credits;
+	this->grade = grade;
+	next = nullptr;
+}
